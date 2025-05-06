b928564e85b2b18ff1e675d59935dfa1cc1b6a1f
shinhyeanseong <ahddud89@gmail.com>
2025-05-07 04:58:27 +0900

    1. 5월2일자 수업 완성

--- Assets/My2D/Scripts/PlayerController.cs
+++ Assets/My2D/Scripts/PlayerController.cs
@@ -3,31 +3,41 @@ using UnityEngine.InputSystem;
 
 namespace My2D
 {
+    //플레이어를 제어하는 클래스
     public class PlayerController : MonoBehaviour
     {
+        #region Variables
+        //강체
         private Rigidbody2D rb2D;
 
+        //이동        
+        //걷는 속도 - 좌우로 걷는다
         [SerializeField] private float walkSpeed = 4f;
-        private Vector2 moveInput;
+
+        //이동 입력값
+        private Vector2 inputMove;
+        #endregion
 
         #region Unity Event Method
         private void Awake()
         {
-            //참조값 가져오기
+            //참조
             rb2D = this.GetComponent<Rigidbody2D>();
         }
+
         private void FixedUpdate()
         {
-            //input 값에 따라 플레이어 좌우 이동
-            rb2D.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb2D.linearVelocity.y);
+            //인풋값에 따라 플레이어 좌우 이동
+            rb2D.linearVelocity = new Vector2(inputMove.x * walkSpeed, rb2D.linearVelocity.y);
         }
         #endregion
 
         #region Custom Method
         public void OnMove(InputAction.CallbackContext context)
         {
-            moveInput = context.ReadValue<Vector2>();
+            inputMove = context.ReadValue<Vector2>();
         }
-        #endregion             
+        #endregion
+
     }
 }
